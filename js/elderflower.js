ce574d5a60849882fbed6da4153f5a7ca3a1e135
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-03-22 16:08:59 -0400

    attempt to replace html tags with content via js

--- js/elderflower.js
+++ js/elderflower.js
@@ -10,12 +10,25 @@ el.textContent = welcomeMessage;
 
 //Title Calculations
 
-var customSign = 'Montague House';
-var quantity = customSign.length;
-var pricePerTile = 5;
-var subtotal = pricePerTile * totalTiles;
-var shipping = 7;
-var grandTotal = subtotal + shipping;
+var customSign = 'Montague House'; //Sign is Montague House
+var quantity = customSign.length; // Quantity is 14
+var pricePerTile = 5; // Price per Tile is 5
+var subtotal = pricePerTile * totalTiles; //Subtotal is 5 * 14
+var shipping = 7; //Shipping is $7
+var grandTotal = subtotal + shipping; //Grand Total is $70 + $7
 
-var el = document.getElementById('customSign');
+var el = document.getElementById('custom-sign');
 el.textContent = customSign;
+
+var el = document.getElementById('quantity');
+el.textContent = quantity;
+
+var el = document.getElementById('subtotal');
+el.textContent = '$' + subtotal;
+
+var el = document.getElementById('shipping');
+el.textContent = '$' + shipping;
+
+var el = document.getElementById('grand-total');
+el.textContent = '$' + grandTotal;
+
