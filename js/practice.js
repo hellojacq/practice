09b7ef3a81dd8f7bbb9e76e5c425af5df78c379a
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-03-25 21:59:01 -0400

    jacq first fizzbuzz

--- js/practice.js
+++ js/practice.js
@@ -21,3 +21,25 @@ function isOdd (number) {
 		return false;
 	}
 }
+
+function fizzbuzz () {
+	for (var i=1; i<=100; i+=1) {
+		if (i % 15 === 0) {
+			document.write("fizzbuzz<br>");
+		} else if (i % 5 === 0) {
+			document.write("buzz<br>");
+		} else if (i % 3 === 0) {
+			document.write("fizz<br>");
+		} else {
+			document.write(i+"<br>");
+		}
+	}
+}
+
+// for (initial; condition; loop statement) {
+  /*
+   statements will be executed every time
+   the for{} loop cycles, while the
+   condition is satisfied
+  */
+ //}
