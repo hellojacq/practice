33ca0fc2d16e145a2c29ecf5e9aaf9edc3f6f1b6
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-03-31 00:51:10 +0000

    added js to footer and made portfolio layout responsive

--- js/responsive.js
+++ js/responsive.js
@@ -1,3 +1,9 @@
 function myFunction() {
   document.getElementById("top-nav").classList.toggle("responsive");
 }
+
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('footer');
+el.innerHTML = '<p>Copyright &copy;' + year + '</p>';
