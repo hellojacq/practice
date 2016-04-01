c93a425b99a94b4f01e091e2f273dd52ab1a9212
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-04-01 01:52:50 +0000

    used Date object to calculate promotional deadline

--- js/hotel.js
+++ js/hotel.js
@@ -8,8 +8,19 @@ document.getElementById('hotelName').textContent = hotel.name;
 document.getElementById('listPrice').textContent = hotel.listPrice;
 document.getElementById('salePrice').textContent = hotel.salePrice;
 
-var today = new Date();
-var day = today.getDay();
-var expiryDate = today.getDate()+7;
+var daysOfWeek = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
-document.getElementById('day').textContent = day;
+var today = new Date();
+var todaysDay = daysOfWeek[today.getDay()];
+var todaysDate = today.toDateString();
+var todaysEpoch = today.getTime();
+var plusSevenDays = todaysEpoch + 604800000;
+var expiry = new Date(plusSevenDays); 
+var expiryDate = expiry.getDate();
+var expiryMonth = months[expiry.getMonth()];
+var expiryYear = (expiry.getYear())+1900;
+var expiryDeadline = '(' + expiryDate + ' ' + expiryMonth + ' ' + expiryYear + ')';
+
+document.getElementById('day').textContent = todaysDay;
+document.getElementById('deadline').textContent = expiryDeadline;
