2b29fc3dc2e0d1ccaa658e45c57ee79ae9d9798f
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-04-12 22:38:29 +0000

    created two reverse functions with reverse method

--- js/data-exercises.js
+++ js/data-exercises.js
@@ -1,5 +1,7 @@
 //sum of a range
 
+/*
+
 function range(start,end,step) {
   var rangeArray= [];
   for (var i=start; (step>0)?(i<=end):(i>=end); i = i+step ) {
@@ -17,3 +19,32 @@ function sum(array) {
 }
 
 console.log(sum(range(5,2,-1)));
+
+
+
+//write two functions, reverseArray and reverseArrayInPlace without using reverse method
+
+function reverseArray(array) {
+  newArray = [];
+  for (var i = array.length-1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+
+*/
+
+var arrayValue = [1, 2, 3, "banana", 5];
+
+function reverseArrayInPlace(arrayValue) {
+  for (var i = 0; i <= Math.floor((arrayValue.length-1)/2); i++) {
+    var temp = arrayValue[i];
+    arrayValue[i] = arrayValue[arrayValue.length-1-i];
+    arrayValue[arrayValue.length-1-i] = temp;  
+  }
+}
+
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
